ece3662d71bf3f8f5f7245d9c311679e10b518fd
mcamilli <marcelo.camilli91@gmail.com>
2021-08-09 16:29:26 -0300

    Exercícios semana1 variáveis adcionados

---
+++ semana1/variaveis/index.js
@@ -0,0 +1,60 @@
+/* Exercícios de interpretação */
+
+let a = 10
+let b = 10
+
+console.log(b)
+
+b = 5
+console.log(a, b)
+
+/* 10  5 */ 
+
+let a = 10
+let b = 20
+c = a
+b = c
+a = b
+
+console.log(a, b, c)
+
+/* 10 10 10 */ 
+
+let p = prompt("Quantas horas você trabalha por dia?")
+let t = prompt("Quanto você recebe por dia?")
+alert(`Voce recebe ${t/p} por hora`)
+
+/* let HorasTrabalhadasPorDia prompt("Quantas horas você trabalha por dia?" 
+   let SalarioPorDia = prompt("Quanto você recebe por dia?"
+console.log ("Voce recebe ${HorasTrabalhadasPordia/SalarioPorDia} por hora") */
+
+
+/* Exercícios de escrita de cógdigo
+    Exercício 1 */
+
+let nome  
+let idade 
+
+console.log (typeof nome, typeof idade)
+
+/* Foi impresso "undefined" pois não atribuímos nenhum valor */ 
+
+let SeuNome = prompt ("Digite seu nome")
+let SuaIdade = prompt ("Digite sua idade")
+
+console.log ("Olá, "SeuNome" , você tem "SuaIdade" anos")
+
+
+/*Exercício 2 */
+
+
+const CorDaRoupa = "Você está usando uma roupa azul hoje ?" 
+const CorDoTenis = "Você está usando um tênis branco hoje?" 
+const Meia = "Você está usando meias hoje?" 
+
+const 
+
+
+/*Exercício 3 */
+
+
