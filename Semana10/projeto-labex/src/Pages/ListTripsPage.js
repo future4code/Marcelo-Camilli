97e74dfb921cd76d624981ad9568418fd87e0743
mcamilli <marcelo.camilli91@gmail.com>
2021-10-13 21:53:44 -0300

    Adicionei a pagina de viagens

--- Semana10/projeto-labex/src/Pages/ListTripsPage.js
+++ Semana10/projeto-labex/src/Pages/ListTripsPage.js
@@ -1,23 +1,57 @@
 import Header from "./Header";
 import { useHistory } from "react-router";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { TripContainer, TripTitle } from "./Styled";
 
 
 export default function ListTripPage() {
-    const history = useHistory ()
+    const [trips, setTrips] = useState([])
+    const history = useHistory()
 
     const goToApplicationFormPage = () => {
         history.push("./ApplicationFormPage")
     }
+
+    const getTrips = () => {
+        axios
+            .get("https://us-central1-labenu-apis.cloudfunctions.net/labeX/darvas/trips")
+            .then((res) => {
+                console.log(res.data)
+                setTrips(res.data.trips)
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    }
+
+    useEffect(() => {
+        getTrips()
+    }, [])
+
+    const tripList = trips.map((trip) => {
         return (
-            <><Header/>
+            <TripContainer>
+                <p>Nome: {trip.name}</p>
+                <p>Descrição: {trip.description}</p>
+                <p>Planeta: {trip.planet}</p>
+                <p>Duração: {trip.durationInDays}</p>
+                <p>Data: {trip.date}</p>
+                <button onClick={goToApplicationFormPage}> Apply </button>
+            </TripContainer>
+        )
+    })
+
+
+    return (
+        <><Header />
+            <TripTitle>Trips</TripTitle>
             <div>
-                TRIPS
+                {tripList}                
             </div>
 
-            <div>
-                <button onClick ={goToApplicationFormPage}> Apply </button>
-            </div>
-            </>
-        );
-    }
+
+        </>
+    );
+}
 
--- Semana10/projeto-labex/src/Pages/Styled.js
+++ Semana10/projeto-labex/src/Pages/Styled.js
@@ -66,3 +66,36 @@ top: 20vh;
 font-size: 36px;
 color: rgb(187, 208, 187);
 ` 
+
+// ---TripListPage ---//
+
+export const TripContainer = styled.div ` 
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+border: 1px solid black;
+margin-right: 40vh;
+margin-left: 40vh;
+padding: 10px;
+
+button {
+    padding: 10px;
+    margin: 10px;
+    background-color: rgb(187, 208, 187);
+    border:0;
+    border-radius: 5px;
+    width: 200px;
+    height: 40px;    
+    font-size: larger;
+}
+`
+
+export const TripTitle = styled.div ` 
+justify-content: center;
+font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+font-size: 36px;
+display: flex;
+margin-top: 50px;
+margin-bottom: 25px;
+`
