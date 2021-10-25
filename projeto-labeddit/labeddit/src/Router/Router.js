06a5c130573fa8e1eaa3ad544c19e59fcc1460f3
mcamilli <marcelo.camilli91@gmail.com>
2021-10-25 20:08:49 -0300

    Adicionei as paginas e o router

---
+++ projeto-labeddit/labeddit/src/Router/Router.js
@@ -0,0 +1,31 @@
+import React from "react"
+import {BrowserRouter, Switch, Route} from "react-router-dom"
+import LoginPage from "../pages/LoginPage/LoginPage"
+import SignupPage from "../pages/SignupPage/SignupPage"
+import FeedPage from "../pages/FeedPage/FeedPage"
+import PostPage from "../pages/PostPage/PostPage"
+
+
+const  Router = () => {
+  return (
+    <BrowserRouter>
+    <Switch>
+        <Route exact path ="/login">
+            <LoginPage/>
+        </Route>
+        <Route exact path ="/signup">
+            <SignupPage/>
+        </Route>
+        <Route exact path ="/">
+            <FeedPage/>
+        </Route>
+        <Route exact path ="/post">
+            <PostPage/>
+        </Route>
+
+    </Switch>
+    </BrowserRouter>
+  )
+}
+
+export default Router
