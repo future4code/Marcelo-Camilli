133dbf91c06ae57103171a812f262b85b7ddcea9
mcamilli <marcelo.camilli91@gmail.com>
2021-10-29 14:39:59 -0300

    Adicionei o a página de comentarios

--- projeto-labeddit/labeddit/src/pages/FeedPage/FeedPage.js
+++ projeto-labeddit/labeddit/src/pages/FeedPage/FeedPage.js
@@ -1,13 +1,49 @@
 import React from "react"
 import useProtectedPage from "../../hooks/useProtectedPage"
+import useRequestData from "../../hooks/useRequestData"
+import {  PostContainer, UserContainer, WritePostContainer, TextContainer, FeedPageContainer, ComentContainer } from "./styled"
+import { BASE_URL } from "../../constants/urls"
+
 
 const  FeedPage = () => {
   useProtectedPage ()
-  return (
-        <div>
+  const posts = useRequestData([], `${BASE_URL}/posts`)
+  console.log(posts)
 
-      Feed
-    </div>
+  const postCards = posts.map ((post) => {
+    return (
+    <><PostContainer>
+      <UserContainer>
+       <strong>
+        {post.username} 
+      </strong>
+      </UserContainer>
+        <TextContainer>
+        {post.title}
+        {post.body}        
+        </TextContainer>
+        <ComentContainer>
+
+          <button>Comentar</button>
+        </ComentContainer>
+    </PostContainer></>
+    
+    )
+  })
+  return (    
+     
+      <>
+      <FeedPageContainer>
+      <WritePostContainer>
+      Novo post
+      <button> Postar</button>
+    </WritePostContainer>
+      <div>
+        {postCards}
+      </div>
+      </FeedPageContainer>
+      </>
+
   )
 }
 
